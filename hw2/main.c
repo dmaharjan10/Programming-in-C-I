3e5a1440bc5eb66a1d7773464edc3fc01efe9078
Drish10 <44743273+ohwhatafool@users.noreply.github.com>
2019-04-04 02:33:53 +0200

    Add files via upload

--- hw2/main.c
+++ hw2/main.c
@@ -3,15 +3,13 @@
 
 int main()
 {
-    int w,d,h;
-    printf("Enter number of weeks");
-    scanf("%d",&w);
-    printf("Enter number of days");
-    scanf("%d",&d);
-    printf("Enter number of hours");
-    scanf("%d",&h);
-    float totalhours;
-    totalhours=w*7*24+d*24+h;
-    printf("Total number of hours is %f\n",totalhours);
+    float a,b,h;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&h);
+    printf("square area=%f\n",a*a);
+    printf("rectangle area=%f\n",a*b);
+    printf("triangle area=%f\n",a*h/2);
+    printf("trapezoid area=%f\n", (a+b)/2*h);
     return 0;
 }
