77276efb99fb1b295592b183be9de42896a127ea
Drish10 <44743273+ohwhatafool@users.noreply.github.com>
2019-04-04 02:31:58 +0200

    Add files via upload

---
+++ hw2/a2p2.c
@@ -0,0 +1,23 @@
+/*
+JTSK-320111
+a2p2.c
+Drishti Maharjan
+d.maharjan@jacobs-university.de
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char a;
+
+scanf("%c",&a);
+
+printf("character=%c\n",a);
+printf("decimal=%d\n",a);
+printf("octal=%o\n",a);
+printf("hexadecimal=%x\n",a);
+
+ return 0;
+
+}
--- hw2/main.c
+++ hw2/main.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    double a,b;
-    scanf("%lf",&a);
-    getchar();
-    scanf("%lf",&b);
-    getchar();
-    printf("sum of doubles=%lf\n",a + b);
-    printf("difference of doubles=%lf\n",a - b);
-    printf ("square=%lf\n",a*a);
-
-    int x,y;
-    scanf("%d",&x);
-    getchar();
-    scanf("%d",&y);
-    getchar();
-    printf("sum of integers=%d\n",x+y);
-    printf("product of integers=%d\n",x*y);
-
-    char w,e;
-    scanf("%c",&w);
-    getchar();
-    scanf("%c",&e);
-    getchar();
-    printf ("sum of chars=%d\n",w+e );
-    printf ("product of chars=%d\n",w*e);
-    printf("sum of chars=%c\n",w+e);
-    printf("product of chars=%c\n",w*e);
-
-
+    int w,d,h;
+    printf("Enter number of weeks");
+    scanf("%d",&w);
+    printf("Enter number of days");
+    scanf("%d",&d);
+    printf("Enter number of hours");
+    scanf("%d",&h);
+    float totalhours;
+    totalhours=w*7*24+d*24+h;
+    printf("Total number of hours is %f\n",totalhours);
     return 0;
 }
