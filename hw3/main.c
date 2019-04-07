df3811c1448ca7c2c0936a99e414d790f859803d
Drish10 <44743273+ohwhatafool@users.noreply.github.com>
2019-04-07 02:27:14 +0200

    Add files via upload

---
+++ hw3/main.c
@@ -0,0 +1,18 @@
+/* JTSK-320111
+3.1.c
+Drishti Maharjan
+d.maharjan@jacobs-university.de */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i = 8;
+      while (i >= 4) {
+      printf("i is %d\n", i);
+      i--;
+      }
+    printf("That’s it.\n");
+
+    return 0;
+}
